a4d4c09968a961513f67a98e418758aa9ab3c346
u-faizan <mianumarzareen@gmail.com>
2026-02-02 17:26:38 +0500

    Pipline configured with DVC with Parameters

--- src/model_building.py
+++ src/model_building.py
@@ -28,7 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -104,7 +119,10 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators': 40, 'random_state': 42}
+        # params = {'n_estimators': 40, 'random_state': 42}
+        params = load_params(params_path='params.yaml')['model_building']
+
+        
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
